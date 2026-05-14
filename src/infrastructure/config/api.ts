cf227e6bb51de33dbc6f0bc0e6e303f09e2e16b5
Pedro Stabile Neto <stabilepedro010403@gmail.com>
2026-05-14 19:36:15 -0300

    variavel para base url

--- src/infrastructure/config/api.ts
+++ src/infrastructure/config/api.ts
@@ -1,1 +1,3 @@
-export const API_BASE_URL = 'http://localhost:5151/api/v1';
+export const API_BASE_URL = import.meta.env.DEV 
+  ? 'http://localhost:5151/api/v1' 
+  : 'https://api.odontech.app.br/api/v1';
